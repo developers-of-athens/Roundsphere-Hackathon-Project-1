254e3f440ced3ccb4dcdc11c074ff23df3f41e5a
Jane Ullah <janeullah@gmail.com>
2015-02-07 11:40:08 -0500

    Added multer and got upload to the directory to work.

--- myapp/routes/uploader.js
+++ myapp/routes/uploader.js
@@ -1,5 +1,21 @@
 var express = require('express');
-var router = express.Router();
+var multer  = require('multer');
+var router  = express.Router();
+var done    = false;
+
+// middleware specific to this router
+router.use(multer({ dest: './uploads/',
+ rename: function (fieldname, filename) {
+    return filename+Date.now();
+  },
+onFileUploadStart: function (file) {
+  console.log(file.originalname + ' is starting ...')
+},
+onFileUploadComplete: function (file) {
+  console.log(file.fieldname + ' uploaded to  ' + file.path)
+  done=true;
+}
+}));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,7 +24,16 @@ router.get('/', function(req, res, next) {
 
 router.post('/pdfupload', function(req,res){
   console.log(req.body);
-  res.send('File uploaded.');
+  var files = req.files.file;
+  if (Array.isArray(files)) {
+    // response with multiple files (old form may send multiple files)
+    console.log("Got " + files.length + " files");
+  }
+  else {
+    // dropzone will send multiple requests per default
+    console.log("Got one file");
+  }
+  res.sendStatus(200);
 })
 
 module.exports = router;
