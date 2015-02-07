cc817d433fb08b6cd7cf545d9a561e03c09356ab
Jane Ullah <janeullah@gmail.com>
2015-02-07 11:22:19 -0500

    saving changes.

--- myapp/routes/uploader.js
+++ myapp/routes/uploader.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('uploader', { uploader: 'File Upload' });
 });
 
+router.post('/pdfupload', function(req,res){
+  console.log(req.body);
+  res.send('File uploaded.');
+})
+
 module.exports = router;
